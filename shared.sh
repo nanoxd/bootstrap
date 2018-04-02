31ecb90e544758211515b25fdf30f8ad045194d6
Fernando Paredes <nano@fdp.io>
2018-04-01 21:21:28 -0400

    ✨ Add shared functions

---
+++ shared.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env sh
+
+# Took these printing functions from https://github.com/Sajjadhosn/dotfiles
+colored_echo() {
+    local exp="$1";
+    local color="$2";
+    local arrow="$3";
+    if ! [[ $color =~ '^[0-9]$' ]] ; then
+       case $(echo $color | tr '[:upper:]' '[:lower:]') in
+        black) color=0 ;;
+        red) color=1 ;;
+        green) color=2 ;;
+        yellow) color=3 ;;
+        blue) color=4 ;;
+        magenta) color=5 ;;
+        cyan) color=6 ;;
+        white|*) color=7 ;; # white or invalid color
+       esac
+    fi
+    tput bold;
+    tput setaf "$color";
+    echo "$arrow $exp";
+    tput sgr0;
+}
+
+info() {
+    colored_echo "$1" blue "\n==>"
+}
+
+success() {
+    colored_echo "$1" green "\n==>"
+}
+
+error() {
+    colored_echo "$1" red "\n==>"
+}
+
+substep_info() {
+    colored_echo "$1" yellow "======"
+}
+
+substep_success() {
+    colored_echo "$1" cyan "======"
+}
+
+substep_error() {
+    colored_echo "$1" red "======"
+}
